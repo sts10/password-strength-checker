0b1be8f40c82a9600f41374449ed289b29c831ee
Sam Schlinkert <sschlinkert@gmail.com>
2023-04-14 00:17:12 -0400

    upgrades zxcvbn crate dependency

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,15 @@ fn main() {
 
         let estimate =
             zxcvbn(&pass, &[&username, "email", "gmail", "twitter", "facebook"]).unwrap();
-        println!("\nScore: {} out of 4\n", estimate.score);
+        println!("\nScore: {} out of 4\n", estimate.score());
         // println!(
         //     "Estimated number of guesses needed to crack: {} (10 ^ {})",
         //     estimate.guesses, estimate.guesses_log10
         // );
 
-        print_guess_time(&estimate.crack_times_display);
+        print_guess_time(&estimate.crack_times());
         println!();
-        give_feedback(estimate.feedback);
+        give_feedback(estimate.feedback());
         println!();
 
         if args.len() > 1 && args[1].contains('l') {
@@ -42,30 +42,30 @@ fn main() {
     }
 }
 
-fn print_guess_time(crack_times: &zxcvbn::time_estimates::CrackTimesDisplay) {
+fn print_guess_time(crack_times: &zxcvbn::time_estimates::CrackTimes) {
     println!(
         "In a throttled online attack:      {}",
-        crack_times.online_throttling_100_per_hour
+        crack_times.online_throttling_100_per_hour()
     );
     println!(
         "In an unthrottled online attack:   {}",
-        crack_times.online_no_throttling_10_per_second
+        crack_times.online_no_throttling_10_per_second()
     );
     println!(
         "In a slowed offline attack:        {}",
-        crack_times.offline_slow_hashing_1e4_per_second
+        crack_times.offline_slow_hashing_1e4_per_second()
     );
     println!(
         "In a fast offline attack:          {}",
-        crack_times.offline_fast_hashing_1e10_per_second
+        crack_times.offline_fast_hashing_1e10_per_second()
     );
 }
 
-fn give_feedback(feedback: Option<zxcvbn::feedback::Feedback>) {
+fn give_feedback(feedback: &Option<zxcvbn::feedback::Feedback>) {
     let spacer = "   ";
     match feedback {
         Some(feedback) => {
-            if let Some(warning) = feedback.warning {
+            if let Some(warning) = feedback.warning() {
                 println!("Warning: {}\n", warning);
             }
             // match feedback.warning {
@@ -73,7 +73,7 @@ fn give_feedback(feedback: Option<zxcvbn::feedback::Feedback>) {
             //     None => (),
             // }
             println!("Suggestions");
-            for suggestion in feedback.suggestions {
+            for suggestion in feedback.suggestions() {
                 println!("{}- {}", spacer, suggestion)
             }
         }
@@ -84,7 +84,7 @@ fn give_feedback(feedback: Option<zxcvbn::feedback::Feedback>) {
 fn gets() -> io::Result<String> {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(_n) => Ok(input.trim_end_matches("\n").to_string()),
+        Ok(_n) => Ok(input.trim_end_matches('\n').to_string()),
         Err(error) => Err(error),
     }
 }
